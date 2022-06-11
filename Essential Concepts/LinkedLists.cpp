8b0f9af820b40eed1f10165a8e8489853b2c13cc
Daniel Villacis <daniel1997v@gmail.com>
2022-06-11 10:59:45 -0400

    starting with linked lists

---
+++ Essential Concepts/LinkedLists.cpp
@@ -0,0 +1,27 @@
+//
+//  LinkedLists.cpp
+//  Essential Concepts
+//
+//  Created by Daniel Villacis on 2022-06-11.
+//
+
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+
+// Typical definition of a node in a single linked list
+struct LinkedList {
+    int val;    // number value inside the node
+    //singlyLinkedList *next;  // Pointer to the next node
+    //singlyLinkedList(int x) : val(x), next(null);
+};
+
+
+
+
+int main() {
+    
+    
+    return 0;
+}
