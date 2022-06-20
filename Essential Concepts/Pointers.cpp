5750d250363c079c54a0f57e1e05fe332bc7b0a5
Daniel Villacis <daniel1997v@gmail.com>
2022-06-20 13:29:06 -0400

    pointers updated

--- Essential Concepts/Pointers.cpp
+++ Essential Concepts/Pointers.cpp
@@ -5,6 +5,7 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <iostream>
 using namespace std;
 
@@ -17,15 +18,65 @@ using namespace std;
 
 int main() {
     
-    // Declaring a pointer vs declaring a variable
+    // Pointer to a variable
     int a = 10; // integer variable
     int *p;     // Pointer
-    
-    p = &a;     // Initializing the pointer : his address will be the cell where a is stored.
-    
-    cout << a;
+    p = &a;
     cout << *p; // This will print the value of the accessed cell, which is a = 10;
     cout << p;  // This will print the hex. address where the pointer is located, such as 0xf135e for example.
     
+    // Pointer to an array : This array is created inside the stack of the memory
+    int A[5] = {2,4,6,8,10};
+    int *q;
+    q = A;      // Assigning the pointer to the array
+    q = &A[0];  // Assigning the pointer to the first element of the array. Note : {p = &A is valid}
+    
+    for(int i=0; i<5; i++) {
+        cout<<A[i];
+        cout<<q[i];
+    }
+    
+    // Pointer to an array : This array is created inside the heap of the memory
+    int *d;
+    d = new int[5];
+    d[0]=10;
+    d[1]=15;
+    d[2]=21;
+    d[3]=14;
+    d[4]=31;
+    
+    for(int i=0; i<5; i++) {
+        cout << d[i];   // will print each value of the pointers of the array inside the heap.
+    }
+    // Since we allocated the extra memory in the heap, we need to delete it after using it (IMPORTANT).
+    delete [] p;
+    
     return 0;
 }
+
+// Pointers can be used anywhere, even with structures
+struct Rectangle {
+    int lenght;
+    int breadth;
+};
+
+int pointerOfStructure() {
+    int * p1;
+    char * p2;
+    float *p3;
+    double *p4;
+    struct Rectangle *p5;
+    cout<< sizeof(p1);
+    cout<< sizeof(p2);
+    cout<< sizeof(p3);
+    cout<< sizeof(p4);
+    cout<< sizeof(p5);
+    
+    // We may think this output will be the size of each data (int = 4bytes, etc.)
+    // but in reality, the output will be 8 bytes. Pointers always take the same amount of memory (8 bytes on last c++ compiler)
+    // since it's its own data type, pointers are independent of the data type stored inside of them.
+    
+    return 0;
+}
+
+
